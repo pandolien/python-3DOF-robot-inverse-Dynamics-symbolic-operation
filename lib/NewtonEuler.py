e5e72c6c64caa476a2e4d0a74da8fe5f11bba5dd
pandolien <pandolia9226@gmail.com>
2025-09-22 23:23:31 +0900

    coriolis Matrix update

--- lib/NewtonEuler.py
+++ lib/NewtonEuler.py
@@ -2,9 +2,17 @@ from casadi import *
 from dhMat import *
 from Inertia import *
 class NE:
-    def __init__(self,H,Jv,Jw,I,mass):
-        self.M = SX.zeros(3,3)
-        self.C = 
+    def __init__(self,n,H,Jv,Jw,I,mass,g,q,dq,CoM):
+        self.M = SX.zeros(n,n)
+        self.C = SX.zeros(n,n)
+        self.G = SX.zeros(n,1)
+        self.n = n
         for i in range(0,len(I)):
             I[i].RIR_T(H[i].R())
             self.M = self.M + mass[i]*(Jv[i].T@Jv[i]) +Jw[i].T@I[i].v@Jw[i]
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    cijk = 0.5*(jacobian(self.M[i,j],q[k]) + jacobian(self.M[i,k],q[j]) - jacobian(self.M[j,k],q[i]))*dq[k]
+                    self.C[i,j] += cijk
+                    
